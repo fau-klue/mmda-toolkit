faca12c9794efbce460c4d296731e1b78b5f9609
Markus Opolka <markus@martialblog.de>
2019-01-19 09:38:47 +0100

    Add more tests for rules

--- mmda-frontend/tests/unit/utils/validation.spec.js
+++ mmda-frontend/tests/unit/utils/validation.spec.js
@@ -14,14 +14,26 @@ describe('Email rules', () => {
   })
 })
 
-describe('Alphanum ruless', () => {
+describe('Alphanum rules', () => {
   it('validate correct string', () => {
     expect(rules.alphanum('happystring')).toBe(true)
     expect(rules.alphanum('VERYHAPPY')).toBe(true)
   })
 
   it('validate invalid string', () => {
-    expect(rules.alphanum('badstring!!!!')).toBe('Invalid characters.')
-    expect(rules.alphanum('(&$*)  !!!!')).toBe('Invalid characters.')
+    expect(rules.alphanum('badstring!!!!')).toMatch('Invalid characters.')
+    expect(rules.alphanum('(&$*)  !!!!')).toMatch('Invalid characters.')
+  })
+})
+
+describe('Max and min length rules', () => {
+  it('validate max min length', () => {
+    expect(rules.counter('thisisok')).toBe(true)
+    expect(rules.counter('thisnooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooot')).toMatch('Max 64 characters.')
+  })
+
+  it('validate invalid max min length', () => {
+    expect(rules.minlength8('muchbetter')).toBe(true)
+    expect(rules.minlength8('not')).toMatch('Min 8 characters.')
   })
 })
