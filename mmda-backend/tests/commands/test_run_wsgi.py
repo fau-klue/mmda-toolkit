22f26d149da63c6e5b6f600ea52cb159713d35ec
Markus Opolka <markus@martialblog.de>
2019-01-24 14:28:06 +0100

    Add GitLab tests for multiple Python versions

--- mmda-backend/tests/commands/test_run_wsgi.py
+++ mmda-backend/tests/commands/test_run_wsgi.py
@@ -5,9 +5,11 @@ from backend.commands.run_wsgi import WSGICommand
 
 
 @mock.patch('backend.commands.run_wsgi.WSGIServer')
-def test_run_wsgi(mock_gevent):
+@mock.patch('backend.commands.run_wsgi.create_app')
+def test_run_wsgi(mock_create, mock_gevent):
 
     server = WSGICommand()
     server.run()
 
-    assert mock_gevent.call_count == 1
+    assert mock_gevent.call_count == 2
+    assert mock_create.call_count == 1
