b92571f3e2b80c70aa9d429e894e35b93fc712f0
Markus Opolka <markus@martialblog.de>
2019-01-25 21:59:53 +0100

    Add framework for internationalization

--- mmda-frontend/src/main.js
+++ mmda-frontend/src/main.js
@@ -4,11 +4,13 @@ import './plugins/vuetify'
 import App from './App.vue'
 import router from './router/router'
 import store from './store/store'
+import i18n from './i18n'
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
+  i18n,
   render: h => h(App)
 }).$mount('#app')
