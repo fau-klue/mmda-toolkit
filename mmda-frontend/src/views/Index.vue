9c04b99c3d56ac6ec94a89323c16a6a245314fdd
Markus Opolka <markus@martialblog.de>
2019-02-06 15:35:58 +0100

    Add index text for english

--- mmda-frontend/src/views/Index.vue
+++ mmda-frontend/src/views/Index.vue
@@ -4,23 +4,14 @@
     <v-flex xs12>
       <h1>Mixed Method Discourse Analysis</h1>
 
-      <p>{{ $t("index.top") }}</p>
-
-      <p>Illo cum quia consequatur ipsam. Accusantium facere et reiciendis explicabo omnis. Fugit sunt corrupti quia in.</p>
-
-      <p>Et nam asperiores ut ratione excepturi sed et. Rem illo laboriosam et. Culpa et ut dolorem esse sit ducimus. Qui in magni aut ut omnis quas blanditiis. Alias sit impedit consectetur quasi hic quod saepe et.</p>
+      <p>{{ $t("index.paragraph1") }}</p>
 
       <v-parallax src="https://cdn.vuetifyjs.com/images/parallax/material.jpg"></v-parallax>
 
-      <p>Sed ut et minus sunt corrupti. Aliquam voluptatem ducimus voluptatibus fuga dolorem nulla maiores aut. Omnis velit consequatur est dignissimos sint qui est.</p>
+      <p>{{ $t("index.paragraph2") }}</p>
 
-      <p>In veniam illum at ullam. Rerum dolorum molestias eaque dolore sint saepe. Quis voluptatibus non distinctio debitis ullam et nihil error. Qui doloremque voluptatum ipsa accusamus nulla sit non quis. Maiores aliquid quis ullam ratione. Omnis nam suscipit ut et.</p>
+      <p>{{ $t("index.paragraph3") }}</p>
 
-      <p>Excepturi dolorem est dignissimos reprehenderit provident et omnis. Consequatur molestiae eveniet impedit ullam hic ut ab dolores. Error ut qui saepe nam rerum est ad asperiores. Id voluptate porro occaecati voluptatem fugiat voluptatibus eos esse. Eligendi vel ex omnis sint earum distinctio excepturi.</p>
-
-      <p>Et nam asperiores ut ratione excepturi sed et. Rem illo laboriosam et. Culpa et ut dolorem esse sit ducimus. Qui in magni aut ut omnis quas blanditiis. Alias sit impedit consectetur quasi hic quod saepe et.</p>
-
-      <p>Sed ut et minus sunt corrupti. Aliquam voluptatem ducimus voluptatibus fuga dolorem nulla maiores aut. Omnis velit consequatur est dignissimos sint qui est.</p>
     </v-flex>
   </v-layout>
 </v-container>
