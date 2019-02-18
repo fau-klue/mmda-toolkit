b79d8e3472f30fabcea1e267e878ae26db8aa133
Markus Opolka <markus@martialblog.de>
2019-02-18 20:10:26 +0100

    Change RandomEngine to new concordance layout

--- mmda-backend/backend/analysis/engines/rand.py
+++ mmda-backend/backend/analysis/engines/rand.py
@@ -64,9 +64,9 @@ class RandomEngine(Engine):
             sentence_length = randint(5,15)
             sentence = random_words(num=sentence_length)
             random_concordance = {'s_pos': 323122,
-                                  'tokens': sentence,
-                                  'emphas': [None] * sentence_length,
-                                  'lemmas': sentence}
+                                  'words': sentence,
+                                  'roles': ['token'] * sentence_length,
+                                  'tt_lemmas': sentence}
             ret_concordances.append(random_concordance)
 
         return ret_concordances
