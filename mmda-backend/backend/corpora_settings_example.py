87101ca5c6b21abc89e91f73a7dce551b2d22ef8
Philipp Heinrich <philipp.heinrich@fau.de>
2019-02-21 12:09:58 +0100

    corpora example settings

--- mmda-backend/backend/corpora_settings_example.py
+++ mmda-backend/backend/corpora_settings_example.py
@@ -3,26 +3,43 @@ Corpus specific settings
 Contains the available corpora on the system
 
 name: Human-readable name
+name_api: Name used in Flask-API
 wectors: Path to pymagnitude word vectors
 language: Language of corpus
 genre: Genre of the corpus
-engine: Engine the corpus uses
+s_att: s-attribute for concordance and collocation delimitation
+p_att: p-attribute for collocation extraction
 association_measures: Available ssociation measures in the Engine
+engine: Engine the corpus uses
 """
 
 CORPORA = {
-    'SZ_SMALL': {
-        'name': 'Süddeutsche Zeitung',
+    'LTWBY2018': {
+        'name': 'Tweets zur Landtagswahl in Bayern 2018',
+        'name_api': 'LTWBY2018',
+        'wectors': '/opt/wectors/magnitude/deTwitterWord2Vec.magnitude',
+        'language': 'de',
+        'genre': 'Social Media Discourse',
+        's_att': 'tweet',
+        'p_att': 'tt_lemma',
+        'association_measures': ['am.simple.ll',
+                                 'am.log.likelihood',
+                                 'am.Dice',
+                                 'am.MI'],
+        'engine': 'CWBEngine'
+    },
+    'SZ_RANDOM': {
+        'name': 'Süddeutsche Zeitung (mit RandomEngine)',
         'name_api': 'SZ_SMALL',
-        'wectors': '/opt/wector/wiki-news-300d-1M-subword.magnitude',
+        'wectors': '/opt/wector/wiki-twitter-300d-1M-subword.magnitude',
         'language': 'de',
         'genre': 'Social Media Discourse',
         'sentence_boundary': 's',
         'association_measures': ['am.simple.ll', 'am.log.likelihood', 'am.Dice,am.MI'],
-        'engine': 'DummyEngine'
+        'engine': 'RandomEngine'
     },
-    'FAZ_SMALL': {
-        'name': 'FAZ Zeitung',
+    'FAZ_DUMMY': {
+        'name': 'FAZ Zeitung (mit DummyEngine)',
         'name_api': 'FAZ_SMALL',
         'wectors': '/opt/wector/wiki-news-300d-1M-subword.magnitude',
         'language': 'de',
