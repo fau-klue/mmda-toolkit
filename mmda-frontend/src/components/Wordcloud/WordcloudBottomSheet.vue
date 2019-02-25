130aef88a620b61c3cebec2068ea0f8e1bc791b6
Jonas Müller <jonas.ej.mueller@fau.de>
2019-02-25 16:57:06 +0100

    add vergessen

---
+++ mmda-frontend/src/components/Wordcloud/WordcloudBottomSheet.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-bottom-sheet hide-overlay v-model="sheet">
+    <ConcordancesKeywordInContextList/>
+  </v-bottom-sheet>
+</template>
+
+<script>
+import { mapGetters, mapActions } from "vuex";
+import ConcordancesKeywordInContextList from '@/components/Concordances/ConcordancesKeywordInContextList.vue'
+
+export default {
+  name: "WordcloudBottomSheet",
+  components: {
+    ConcordancesKeywordInContextList,
+  },
+  data: () => ({
+    sheet:true,
+  }),
+  computed: {
+    ...mapGetters({
+      concordances: 'corpus/concordances'
+    })
+  },
+  watch:{
+    concordances(){
+      this.sheet=true;
+    }
+  },
+  methods: {
+    ...mapActions({
+    }),
+  }
+};
+</script>
+
+<style>
+</style>
