4cd32c364a0daccd8e1edc9fa1767700c4e7125f
Jonas Müller <jonas.ej.mueller@fau.de>
2019-02-27 15:10:32 +0100

    fixed height of bottom-sheet

--- mmda-frontend/src/components/Wordcloud/WordcloudBottomSheet.vue
+++ mmda-frontend/src/components/Wordcloud/WordcloudBottomSheet.vue
@@ -1,15 +1,15 @@
 <template>
   <v-bottom-sheet class="wordcloud-bottom-sheet" hide-overlay v-model="sheet">
-    <v-card class="card">
+    <v-card class="wordcloud-bottom-card">
       <ConcordancesKeywordInContextList v-bind:concordances="concordances"/>
     </v-card>
   </v-bottom-sheet>
 </template>
 
 <style>
-.wordcloud-bottom-sheet .card{
-  max-height: 50%;
-  overflow: scroll; /*does not work like that*/
+.wordcloud-bottom-card{
+  max-height: 50%;   /*does not work like that, because parents have no height.   -> therefore max-height is set on mounted*/
+  overflow: auto; 
 }
 </style>
 
@@ -38,6 +38,12 @@ export default {
   methods: {
     ...mapActions({
     }),
+  },
+  mounted(){
+    var E = document.getElementsByClassName("wordcloud-bottom-card");
+    for(var e of E){
+      e.style.maxHeight  = Math.floor(innerHeight*0.5)+"px";
+    }
   }
 };
 </script>
