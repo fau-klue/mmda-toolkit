56a5f0974ffc6ff59b820094ba15b8f1933ff40d
Markus Opolka <markus@martialblog.de>
2019-04-27 12:12:07 +0200

    Add endpoint to delete user coordinates

--- mmda-backend/backend/views/coordinates_views.py
+++ mmda-backend/backend/views/coordinates_views.py
@@ -86,7 +86,7 @@ def reload_coordinates(username, analysis):
 def update_coordinates(username, analysis):
     """
     Update the coordinates for an analysis.
-    To Change user_x and user_y.
+    To change user_x and user_y.
     """
 
     if not request.is_json:
@@ -118,3 +118,47 @@ def update_coordinates(username, analysis):
 
     log.debug('Updated semantic space for analysis %s', analysis)
     return jsonify({'msg': 'Updated'}), 200
+
+
+# DELETE
+@coordinates_blueprint.route('/api/user/<username>/analysis/<analysis>/coordinates/', methods=['DELETE'])
+@user_required
+def delete_coordinates(username, analysis):
+    """
+    delete the coordinates for an analysis.
+    To change user_x and user_y.
+    """
+
+    if not request.is_json:
+        log.debug('No coordinate data provided')
+        return jsonify({'msg': 'No request data provided'}), 400
+
+    # TODO Validate request. Should be:
+    # {foo: {user_x: 1, user_y: 2}, bar: {user_x: 1, user_y: 2}}
+    items = request.get_json()
+
+    # Get User
+    user = User.query.filter_by(username=username).first()
+
+    # Get Analysis from DB
+    analysis = Analysis.query.filter_by(id=analysis, user_id=user.id).first()
+    if not analysis:
+        log.debug('No such analysis %s', analysis)
+        return jsonify({'msg': 'No such analysis'}), 404
+
+    # Load Coordinates from DB
+    coordinates = Coordinates.query.filter_by(analysis_id=analysis.id).first()
+    df = coordinates.data
+
+    for item in items.keys():
+        if item in df.index:
+            log.debug('Removing user coordinates for %s', item)
+            df.loc[item]['user_x'] = None
+            df.loc[item]['user_y'] = None
+
+    # Update coordinates dataframe, and save
+    coordinates.data = df
+    db.session.commit()
+
+    log.debug('Updated semantic space for analysis %s', analysis)
+    return jsonify({'msg': 'Deleted'}), 200
