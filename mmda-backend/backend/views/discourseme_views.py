b09cb43c5d6360bfc755199cee8427ba92965991
Markus Opolka <markus@martialblog.de>
2019-06-20 10:32:18 +0200

    Fix bug in Discourseme endpoint
     - Update with no such discourseme failed

--- mmda-backend/backend/views/discourseme_views.py
+++ mmda-backend/backend/views/discourseme_views.py
@@ -99,6 +99,9 @@ def update_discourseme(username, discourseme):
 
     # Get Discourseme from DB
     discourseme = Discourseme.query.filter_by(id=discourseme, user_id=user.id).first()
+    if not discourseme:
+        log.debug('No such discourseme %s', discourseme)
+        return jsonify({'msg': 'No such discourseme'}), 404
 
     if discourseme.topic:
         log.debug('Cannot edit topic discourseme %s', discourseme)
