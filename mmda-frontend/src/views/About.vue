76c03dd9041d1bce6ed9fd8561263a13313b4eef
Philipp Heinrich <philipp.heinrich@fau.de>
2019-10-05 20:32:41 +0200

    meta-text / towards a manual

--- mmda-frontend/src/views/About.vue
+++ mmda-frontend/src/views/About.vue
@@ -3,16 +3,23 @@
   <v-layout row wrap>
     <v-flex xs12>
 
+      <h1>{{ $t("about.headline") }}</h1>
+      <p/>
+      <h2>{{ $t("about.headline1") }}</h2>
+      <p>{{ $t("about.paragraph1_1") }}</p>
+      <h3>{{ $t("about.headline1_1") }}</h3>
+      <p>{{ $t("about.paragraph1_2") }}</p>
+      <p>{{ $t("about.paragraph1_3") }}</p>
+      <p>{{ $t("about.paragraph1_4") }}</p>
+
       <h3>{{ $t("about.headline2") }}</h3>
       <p>{{ $t("about.paragraph2_1") }}</p>
       <p>{{ $t("about.paragraph2_2") }}</p>
+      <p>{{ $t("about.paragraph2_3") }}</p>
 
       <h3>{{ $t("about.headline3") }}</h3>
       <p>{{ $t("about.paragraph3_1") }}</p>
       <p>{{ $t("about.paragraph3_2") }}</p>
-
-      <h3>{{ $t("about.headline4") }}</h3>
-      <p>{{ $t("about.paragraph4_1") }}</p>
 
     </v-flex>
   </v-layout>
