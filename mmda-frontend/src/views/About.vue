22cc91240bef0b5f25daa5573a55bdd85566bd84
Philipp Heinrich <philipp.heinrich@fau.de>
2021-06-17 13:47:32 +0200

    update index and about

--- mmda-frontend/src/views/About.vue
+++ mmda-frontend/src/views/About.vue
@@ -5,20 +5,29 @@
 
       <h1>{{ $t("about.headline") }}</h1>
       <p/>
+
       <h3>{{ $t("about.headline1") }}</h3>
       <p>{{ $t("about.paragraph1_1") }}</p>
-      <h3>{{ $t("about.headline1_1") }}</h3>
+
+      <!-- <h3>{{ $t("about.headline1_1") }}</h3> -->
+
+      <h3>Discoursemes and their Constellations</h3>
+      <p>We call any set of words formed by the analyst a <b>discourseme</b> – they are the building blocks of CDA. Theoretically speaking, a discourseme is a disjunction of related lexical items. On its surface, it represents a simple list of lexical items (e.g. words), which the researcher decided that belong together. The list of topic nodes form a special discourseme – the topic discourseme.</p>
+      <p>CDA researchers are typically interested in constellations of discoursemes: e.g. a topic discourseme <i>nuclear energy</i> and a further discourseme capturing an attitude or perspective towards the topic, such as its <i>risks</i> or its <i>potential</i>. </p>
+
+      <!-- <h3>{{ $t("about.headline2") }}</h3> -->
+      <h3>Creating a Discourseme Analysis</h3>
       <p>{{ $t("about.paragraph1_2") }}</p>
       <p>{{ $t("about.paragraph1_3") }}</p>
-      <p>{{ $t("about.paragraph1_4") }}</p>
-
-      <h3>{{ $t("about.headline2") }}</h3>
+      <h3>Inspecting Analysis Results</h3>
       <p>{{ $t("about.paragraph2_1") }}</p>
-      <p>{{ $t("about.paragraph2_2") }}</p>
-      <p>{{ $t("about.paragraph2_3") }}</p>
 
-      <h3>{{ $t("about.headline3") }}</h3>
-      <p>{{ $t("about.paragraph3_1") }}</p>
+      <h3>The Semantic Map</h3>
+      <p>{{ $t("about.paragraph2_2") }}</p>
+      <!-- <p>{{ $t("about.paragraph2_3") }}</p> -->
+
+      <!-- <h3>{{ $t("about.headline3") }}</h3> -->
+      <!-- <p>{{ $t("about.paragraph3_1") }}</p> -->
       <p>{{ $t("about.paragraph3_2") }}</p>
 
       <h3>{{ $t("about.headline4") }}</h3>
--- mmda-frontend/src/views/Index.vue
+++ mmda-frontend/src/views/Index.vue
@@ -7,22 +7,24 @@
       <p>{{ $t("index.paragraph2") }}</p>
       <p> <v-img src= "mmda-in-action.png"></v-img> </p>
       <p>{{ $t("index.paragraph3") }}</p>
-      <p>{{ $t("index.paragraph4") }}</p>
+      <!-- <p>{{ $t("index.paragraph4") }}</p> -->
+      <p>See the <a href="/mmda/about">Vignette</a> for further info or start with an <a href="/mmda/analysis/new">Analysis</a>.
 
-      <h3>{{ $t("index.headlinePublications") }}</h3>
-      <p>
-        <ul>
-          <li>Evert, Stefan and Philipp Heinrich (2019). Introducing MMDA: An interactive toolkit for CDA. 7th Göttingen/Hildesheim-Workshop on CL and DH.</li>
-          <li>Heinrich, Philipp and Fabian Schäfer (2018). Extending Corpus-Based Discourse Analysis for Exploring Japanese Social Media. Proceedings of 4th Asia Pacific Corpus Linguistics Conference (APCLC2018), pages 135–140. Takamatsu, Japan.</li>
-          <li>Heinrich, Philipp, Christoph Adrian, Olena Kalashnikova, Fabian Schäfer, and Stefan Evert (2018). A Transnational Analysis of News and Tweets about Nuclear Phase-Out in the Aftermath of the Fukushima Incident. In Proceedings of the LREC 2018 “Workshop on Computational Impact Detection from Text Data” (CIDTD 2018), pages 8–16. Miyazaki, Japan.</li>
-......</ul>
-      </p>
-
-      <h3>{{ $t("index.headlineAcknowledgements") }}</h3>
+      <h4>{{ $t("index.headlineAcknowledgements") }}</h4>
       <p>{{ $t("index.Acknowledgements") }}</p>
 
-      <h3>Developers</h3>
-      <p><a href="https://linguistik.uni-erlangen.de/projects/efe/team">Chair of Computational Corpus Linguistics</a>, FAU Erlangen-Nürnberg</p>
+      <h4>Developers</h4>
+      <p><a href="https://linguistik.uni-erlangen.de/projects/efe/team">EFE project group</a>, FAU Erlangen-Nürnberg</p>
+
+      <!-- <h3>{{ $t("index.headlinePublications") }}</h3> -->
+      <!-- <p> -->
+        <!--   <ul> -->
+      <!--     <li>Evert, Stefan and Philipp Heinrich (2019). Introducing MMDA: An interactive toolkit for CDA. 7th Göttingen/Hildesheim-Workshop on CL and DH.</li> -->
+      <!--     <li>Heinrich, Philipp and Fabian Schäfer (2018). Extending Corpus-Based Discourse Analysis for Exploring Japanese Social Media. Proceedings of 4th Asia Pacific Corpus Linguistics Conference (APCLC2018), pages 135–140. Takamatsu, Japan.</li> -->
+      <!--     <li>Heinrich, Philipp, Christoph Adrian, Olena Kalashnikova, Fabian Schäfer, and Stefan Evert (2018). A Transnational Analysis of News and Tweets about Nuclear Phase-Out in the Aftermath of the Fukushima Incident. In Proceedings of the LREC 2018 “Workshop on Computational Impact Detection from Text Data” (CIDTD 2018), pages 8–16. Miyazaki, Japan.</li> -->
+      <!--   </ul> -->
+      <!-- </p> -->
+      
     </v-flex>
   </v-layout>
 </v-container>
