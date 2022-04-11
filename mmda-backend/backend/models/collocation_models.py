998453c230aebf5633f42cbc23ed4ffe9e842810
Philipp Heinrich <philipp.heinrich@fau.de>
2022-04-11 15:28:04 +0200

    bugfix in collocation model

--- mmda-backend/backend/models/collocation_models.py
+++ mmda-backend/backend/models/collocation_models.py
@@ -57,7 +57,7 @@ class Collocation(db.Model):
     corpus = db.Column(db.Unicode(255), nullable=False)
     p_query = db.Column(db.Unicode(255), nullable=False)
     flags_query = db.Column(db.Unicode(255), nullable=False)
-    escape_query = db.Column(db.Unicode(255), nullable=False)
+    escape_query = db.Column(db.Boolean, nullable=False)
     p_collocation = db.Column(db.Unicode(255), nullable=False)
     s_break = db.Column(db.Unicode(255), nullable=False)
     context = db.Column(db.Integer, nullable=True)
