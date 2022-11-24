0e105c3b7723b935932709dc75ca85098dbca4a8
Philipp Heinrich <philipp.heinrich@fau.de>
2022-11-24 20:17:37 +0100

    add small word embeddings

--- mmda-backend/tests/corpora/corpora.py
+++ mmda-backend/tests/corpora/corpora.py
@@ -9,7 +9,7 @@ CORPORA = {
     'GERMAPARL1386': {
         'name': 'GermaParl (small test corpus)',
         'name_api': 'GERMAPARL1386',
-        'embeddings': '/home/ausgerechnet/corpora/embeddings/magnitude/deWikiWord2Vec.magnitude',
+        'embeddings': 'tests/corpora/deWiki-small.magnitude',
         'language': 'de',
         'register': 'standard',
         'description': 'Bundestag'
